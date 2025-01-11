07b58a20eafddc3b45646b6f8e8486863ea6da19
hato-taka <n28hashimoto@gmail.com>
2025-01-11 19:57:19 +0900

    Linkの追加

--- src/server.js
+++ src/server.js
@@ -1,17 +1,34 @@
 const { ApolloServer, gql } = require("apollo-server")
 
+// HackerNewsの投稿
+let links = [
+    {
+        id: "link-0",
+        description: "テストの投稿です",
+        url: ".com"
+    }
+]
+
 // GraphQLスキーマを定義する
 // スキーマとはデータ構造のこと
 const typeDefs = gql`
     type Query {
         info: String!
+        feed: [Link]!
+    }
+    
+    type Link {
+        id: ID!
+        description: String!
+        url: String!
     }
 `
 // リゾルバ関数
 // 定義した値に対して何かしらの実際な値を入れること
 const resolvers = {
     Query: {
-        info: () => "HackerNewsクローン"
+        info: () => "HackerNewsクローン",
+        feed: () => links,
     }
 }
 
