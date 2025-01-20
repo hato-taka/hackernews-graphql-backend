8823bf34bb0e407d56e3cb536fe6db7a5247d472
hato-taka <n28hashimoto@gmail.com>
2025-01-21 04:11:37 +0900

    contextにuserIdを追加した

--- src/server.js
+++ src/server.js
@@ -1,40 +1,45 @@
-const { ApolloServer, gql } = require("apollo-server")
-const fs = require("fs")
-const path = require("path")
+const { ApolloServer, gql } = require("apollo-server");
+const fs = require("fs");
+const path = require("path");
 
-const { PrismaClient } = require("@prisma/client")
-const { url } = require("inspector")
+const { PrismaClient } = require("@prisma/client");
+const { url } = require("inspector");
+const { getUserId } = require("./utils");
 
 // リゾルバ関数
 // 定義した値に対して何かしらの実際な値を入れること
 const resolvers = {
-    Query: {
-        info: () => "HackerNewsクローン",
-        feed: async(parent, args, context) => {
-            return context.prisma.link.findMany()
-        },
+  Query: {
+    info: () => "HackerNewsクローン",
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany();
     },
+  },
 
-    Mutation: {
-        post: (parent, args, context) => {
-            const newLink = context.prisma.link.create({
-                data: {
-                    url: args.url,
-                    description: args.description
-                }
-            })
-            return newLink
-        }
-    }
-}
+  Mutation: {
+    post: (parent, args, context) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      });
+      return newLink;
+    },
+  },
+};
 
 // contextは「どこでも使える」という意味
 const server = new ApolloServer({
-    typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf-8"),
-    resolvers,
-    context: {
-        prisma: new PrismaClient()
-    }
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf-8"),
+  resolvers,
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma: new PrismaClient(),
+      userId: req && req.headers.authorization ? getUserId(req) : null,
+    };
+  },
 });
 
-server.listen().then(({url}) => console.log(`${url}でサーバーが起動中です`))
+server.listen().then(({ url }) => console.log(`${url}でサーバーが起動中です`));
